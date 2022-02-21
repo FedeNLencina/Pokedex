d7e54a00647d738bcf826e4fd169b6073d5e7e7b
Federico Lencina <federiconahuellencina@gmail.com>
2022-02-21 18:44:59 -0300

    se termina el responsive

--- src/components/PokeCell/PokeCell.js
+++ src/components/PokeCell/PokeCell.js
@@ -7,7 +7,7 @@ export default function PokeCell({ pokeClass, handleOnClick}) {
   
     return (
             <button onClick={() => handleOnClick(pokeClass + 1)} className="container poke-cell">
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeClass + 1}.png`} alt={pokeClass}></img>
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeClass + 1}.png`} alt={pokeClass} className="pokemonImg"></img>
             </button>
         
     );
